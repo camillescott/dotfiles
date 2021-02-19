f1107606b06da536d0b4f16a2509a4adb6374ac4
Camille Scott <camille.scott.w@gmail.com>
2021-02-19 15:08:52 -0800

    put alias custom commands in plugin

--- dotfiles.plugin.zsh
+++ dotfiles.plugin.zsh
@@ -1,3 +1,8 @@
 ln -fs $PWD/vimrc $HOME/.vimrc
 ln -fs $PWD/condarc $HOME/.condarc
+
+alias div="tput sgr 0; tput bold; tput setaf 4; printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =; tput sgr 0"
+alias smalldiv="tput bold; tput setaf 4; echo '********************' | center; tput sgr 0"
+alias center="sed  -e :a -e 's/^.\{1,'"$(( $COLS - 2 ))"'\}$/ & /;ta'"
+
 source zshrc
