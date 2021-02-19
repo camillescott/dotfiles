72b1b1866084df6fe3e91a2281f1f74e5c062bab
Camille Scott <camille.scott.w@gmail.com>
2021-02-19 15:26:31 -0800

    cols

--- dotfiles.plugin.zsh
+++ dotfiles.plugin.zsh
@@ -1,7 +1,7 @@
 ln -fs vimrc $HOME/.vimrc
 ln -fs condarc $HOME/.condarc
 
-COLS=`tput cols`
+export COLS=`tput cols`
 alias center="sed  -e :a -e 's/^.\{1,'"$(( $COLS - 2 ))"'\}$/ & /;ta'"
 alias div="tput sgr 0; tput bold; tput setaf 4; printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =; tput sgr 0"
 alias smalldiv="tput bold; tput setaf 4; echo '********************' | center; tput sgr 0"
