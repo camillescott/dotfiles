3537d18b88e1f8de95f3ca684e8c1cf6e4fc5a13
Camille Scott <cswel@ucdavis.edu>
2026-06-18 23:34:38 -0700

    spacing

--- hippo-user.accounts.zsh
+++ hippo-user.accounts.zsh
@@ -2,4 +2,6 @@
 #conda activate /cvmfs/hpc.ucdavis.edu/sw/conda/environments/cheeto
 #cd ~/.local/share/accounts.hpc.ucdavis.edu
 
+echo
 echo WARNING: hippo-user no longer manages cheeto. cheeto operations are now managed by cheeto-user.
+echo
